ea7a2ec3785f93caa7a8b78c493f22c08b17f900
Alexey163 <Alexeevalexey163@gmail.com>
2016-09-20 18:52:03 +0400

    По пробовал воспроизвести событие по клику

--- src/SimpleGui1.java
+++ src/SimpleGui1.java
@@ -1,4 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+//реализовываем интерфейс
+public class SimpleGui1 implements ActionListener {
+    JButton button;
 
-public class SimpleGui1 {
+    public static void main (String [] args) {
+        SimpleGui1 gui1 = new SimpleGui1();
+        gui1.go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("нажми меня");
+
+        //Регистрируем слушатель
+        button.addActionListener(this);
+
+        //описание создоваемого окна и добаление в него кнопки
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    //реализация метода обработки события
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        button.setText("Меня нажали!");
+    }
 }
