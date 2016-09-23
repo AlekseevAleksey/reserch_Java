43af77e8543016d6509de81ec0fa1898d8778d12
Alexey163 <Alexeevalexey163@gmail.com>
2016-09-23 11:23:21 +0400

    research animation and work with subclasses

--- src/SimpleGui1.java
+++ src/SimpleGui1.java
@@ -1,16 +1,14 @@
 import javax.swing.*;
-import java.awt.event.*;
 import java.awt.*;
 
 //реализовываем интерфейс
 public class SimpleGui1 {
-    JButton buttonColor;
-    JButton buttonLabel;
-    JFrame frame;
-    JLabel label;
-    int x;
 
-    public static void main (String [] args) {
+    JFrame frame;
+    int x = 30;
+    int y = 30;
+
+    public static void main(String[] args) {
         SimpleGui1 gui1 = new SimpleGui1();
         gui1.go();
     }
@@ -19,44 +17,40 @@ public class SimpleGui1 {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-        //Создаем кноаку и регистрируем слушатель
-        buttonColor = new JButton("Изменить цвет");
-        buttonColor.addActionListener(new ColorListener());
-
-        buttonLabel = new JButton("Измениь label");
-        buttonLabel.addActionListener(new LabelListener());
-
         //Создаем панель для рисования
         MyDrawPanel drawPanel = new MyDrawPanel();
 
-        //
-        label = new JLabel("Label 0");
-
         //описание создоваемого окна и добаление в него кнопки и панели для рисования
-        frame.getContentPane().add(BorderLayout.SOUTH, buttonColor);
-        frame.getContentPane().add(BorderLayout.EAST, buttonLabel);
+
         frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
-        frame.getContentPane().add(BorderLayout.WEST, label);
-        frame.setSize(500,300);
+        frame.setSize(400, 300);
         frame.setVisible(true);
-    }
 
-    class LabelListener implements ActionListener {
+        //инкриментируем координаты круга
+        for (int i = 0; i < 130; i++) {
+            x++;
+            y++;
+            drawPanel.repaint();
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            x = (int) (Math.random() * 7);
-            label.setText("new label " + x);
+            //замедляем
+            try {
+                Thread.sleep(50);
+            } catch (Exception e) {
+            }
         }
     }
 
-    class ColorListener implements ActionListener {
+    class MyDrawPanel extends JPanel {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            frame.repaint();
+        public void paintComponent(Graphics g) {
+
+            //закрашиваем панель
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+
+            //рисуем круг в новых кооринатах
+            g.setColor(Color.cyan);
+            g.fillOval(x,y, 100,100);
         }
     }
 }
